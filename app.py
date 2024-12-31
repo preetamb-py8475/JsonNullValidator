9b46d2046c6100dd4937db341f4aef893efdad74
Preetam Baruah <47924746+preetamb-py8475@users.noreply.github.com>
2024-12-31 21:43:00 +0530

    Turned OFF Debug mode

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def validate():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run()
